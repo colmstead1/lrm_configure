70ba3b6e681bfe93e8e5af4de6dc9f37e12721bd
Virginia Claire Olmstead <colmstead@lingoport.com>
2020-03-24 15:51:53 -0700

    Updates

---
+++ precache-manifest.ab7ec372b8dd56d1f5a5e8666b599162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c53a355a4e26bc9957cc425381637d7",
+    "url": "/myEmotionality/login/index.html"
+  },
+  {
+    "revision": "ec6f04c2778fa35e93d2",
+    "url": "/myEmotionality/login/static/css/main.d150f867.chunk.css"
+  },
+  {
+    "revision": "3f4708b8b92160af3a0d",
+    "url": "/myEmotionality/login/static/js/2.9900e0e8.chunk.js"
+  },
+  {
+    "revision": "e2bf01c2138e2c05b38ee9c222c01901",
+    "url": "/myEmotionality/login/static/js/2.9900e0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6f04c2778fa35e93d2",
+    "url": "/myEmotionality/login/static/js/main.a75b86ec.chunk.js"
+  },
+  {
+    "revision": "8e0a9361059b8dc64080",
+    "url": "/myEmotionality/login/static/js/runtime-main.61703aa0.js"
+  }
+]);
